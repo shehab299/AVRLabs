b5c9ef1ebf322e134710c420fa0f99caabdba95b
AhmedHamed3699 <ahmed.hamed03@eng-st.cu.edu.eg>
2025-05-01 20:04:24 +0300

    Drivers: Added ultrasonic sensor driver

--- src/drivers/HC-SR04/HCSR04.h
+++ src/drivers/HC-SR04/HCSR04.h
@@ -1,75 +1,60 @@
 #ifndef HCSR04_H
 #define HCSR04_H
-// #define TCNT1 0x85  DO WE REALLY NEED THIS?!!
-#include <util/delay.h>
+
 #include "../utils/bit_utils.h"
+#include "../utils/timer.h"
 
 #define F_CPU 16000000
+#define SOUND_SPEED 34300.0f
 #define TRIG_PIN PB0
 #define ECHO_PIN PB1
 
 class HCSR04
 {
 private:
-    PRESCALER prescaler_;
-    
+    PRESCALER prescaler;
+    Timer *timer;
+    int triggerPin, echoPin;
+
 public:
-    HCSR04(PRESCALER prescaler) : prescaler_(prescaler)
+    HCSR04(int triggerPin = TRIG_PIN, int echoPin = ECHO_PIN, PRESCALER prescaler = PRESCALER_1)
+        : triggerPin(triggerPin), echoPin(echoPin), prescaler(prescaler)
     {
-        switch (prescaler_)
-        {
-        case PRESCALER_1:
-            SET_BITS(TCCR1B, (1 << CS10));
-            break;
-        case PRESCALER_8:
-            SET_BITS(TCCR1B, (1 << CS11));
-            break;
-        case PRESCALER_64:
-            SET_BITS(TCCR1B, (1 << CS11) | (1 << CS10));
-            break;
-        case PRESCALER_256:
-            SET_BITS(TCCR1B, (1 << CS12));
-            break;
-        case PRESCALER_1024:
-            SET_BITS(TCCR1B, (1 << CS12) | (1 << CS11) | (1 << CS10));
-            break;
-        default:
-            break;
-        }
-        SET_BIT(DDRB, TRIG_PIN);
-        CLEAR_BIT(DDRB, ECHO_PIN);
-        TCNT1 = 0;
+        SET_BIT(DDRB, triggerPin);
+        CLEAR_BIT(DDRB, echoPin);
+        SET_BIT(PORTB, triggerPin);
+        timer = new Timer(F_CPU, prescaler);
     }
 
-    void init() { //JUST IN CASE
-        SET_BIT(DDRB, TRIG_PIN);
-        CLEAR_BIT(DDRB, ECHO_PIN);
+    float computeEcho() {
+        float start = timer->getCurrentTime();
+        while (READ_BIT(PINB, echoPin));
+        float end = timer->getCurrentTime();
+        return (end - start) * (prescaler / F_CPU);
     }
 
-    float measureDistance() {
-        CLEAR_BIT(PORTB, TRIG_PIN);
-        _delay_us(2);
-        SET_BIT(PORTB, TRIG_PIN);
-        _delay_us(10);
-        CLEAR_BIT(PORTB, TRIG_PIN);
-        while(!(PINB & (1 << ECHO_PIN)));
-        
-        TCNT1 = 0;
-        
-        // Wait for echo end
-        while(PINB & (1 << ECHO_PIN)) {
-            if(TCNT1 > 60000) {  // Timeout after ~30ms
-                TCCR1B = 0;
-                return -1.0;
-            }
+    float measureDistance(int timeout = 30000) {
+        timer->setDelay(1);
+        SET_BIT(PORTB, triggerPin);
+        timer->setDelay(1);
+        CLEAR_BIT(PORTB, triggerPin);
+    
+        int startTime = timer->getCurrentTime();
+    
+        while (!READ_BIT(PINB, echoPin) && timer->getCurrentTime() - startTime < timeout);
+        if (timer->getCurrentTime() - startTime >= timeout) {
+            return -1;
         }
         
-        uint16_t count = TCNT1;
-        TCCR1B = 0;
+        float travelTime = computeEcho();
+        return (travelTime * SOUND_SPEED) / 2;
+    }
 
-        // Distance = (Time * Sound Speed) / 2 = (count * prescaler * Sound Speed) / 2 * F_CPU 
-        float dist = (float)count * prescaler_ * 34300.0f / (F_CPU * 2.0f);
-        return dist;
+    float measureSpeed(int timeout = 30000) {
+        float start = measureDistance(timeout);
+        timer->setDelay(1000);
+        float end = measureDistance(timeout);
+        return (end - start) / 1.0;
     }
 };
 
